09f00c43dc7c690f72d7bb7fb72ed01027953516
Irwan <irwansyt23@gmail.com>
2024-04-19 15:33:00 +0700

    update by wann

--- api/index.ts
+++ api/index.ts
@@ -1,43 +1,40 @@
-const express = require("express");
-const cors = require('cors');
-const secure = require('ssl-express-www');
-const path = require('path');
+const express = require("express"),
+	cors = require('cors'),
+	secure = require('ssl-express-www');
 const app = express();
-const { color } = require('../lib/color.js');
+const path = require('path');
+var { color } = require('../lib/color.js')
 
 app.enable('trust proxy');
-app.set("json spaces", 2);
-app.use(cors());
-app.use(secure);
-app.use(express.static("public"));
+app.set("json spaces", 2)
+app.use(cors())
+app.use(secure)
+app.use(express.static("public"))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../view/index.html'));
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '../view/index.html'));
+	
 });
-
-app.get('/dosc', (req, res) => {
-    res.sendFile(path.join(__dirname, '../view/dosc.html'));
+app.get('/dosc', function (req, res) {
+	res.sendFile(path.join(__dirname, '../view/dosc.html'));
 });
-
 app.use('/api', require("./api.ts"));
 
 app.get('/config', (req, res) => {
     const config = {
         status: true,
         result: {
-            prefix: '.',
+            prefix : '.',
             namabot: 'Ceria-Bot',
             namaowner: 'Wann',
             instagram: 'irwns23_',
-            youtube: 'Gak Punya'
+            youtube : 'Gak Punya'
         }
-    };
-    res.json(config);
-});
+    }
+    res.json(config)
+})
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(color(`Server running on port ${port}`, 'green'));
-});
+
+app.listen(3000, () => console.log(color("Server running on port 3000",'green')));
 
 module.exports = app;
